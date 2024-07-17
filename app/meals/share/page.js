589224231f9dc2f31956d5e69476c774df632379
htrobles <hjt.robles@gmail.com>
2024-07-16 22:50:08 -0400

    move shareMeal to new file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,10 @@
+'use client';
+
 import ImagePicker from '@/components/meals/imagePicker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-  const shareMeal = async (formData) => {
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      email: formData.get('email'),
-    };
-
-    console.log(meal);
-  };
-
   return (
     <>
       <header className={classes.header}>
