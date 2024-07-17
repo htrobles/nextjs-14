979194efe3bcd2cff96901f7ce2ddbe6f069b20f
htrobles <hjt.robles@gmail.com>
2024-07-16 21:48:58 -0400

    react suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/mealsGrid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +25,11 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals..</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
