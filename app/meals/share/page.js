e0f86538253536abfa9bb23afb8cb1dcccb88eec
htrobles <hjt.robles@gmail.com>
2024-07-16 23:21:08 -0400

    meal submit loading

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,6 +3,7 @@
 import ImagePicker from '@/components/meals/imagePicker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/mealsFormSubmit';
 
 export default function ShareMealPage() {
   return (
@@ -44,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label='Your image' name='image' />
           <p className={classes.actions}>
-            <button type='submit'>Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
